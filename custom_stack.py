c1dffad9a495f843f2a8afb84a20c4d5d896c811
Jamie Bryson <jamieab98@gmail.com>
2026-04-13 22:58:34 -0400

    Got the stack to work

--- custom_stack.py
+++ custom_stack.py
@@ -4,4 +4,25 @@ def is_valid_parentheses(s: str) -> bool:
     Only (), {}, and [] are considered valid.
     """
     # TODO: Implement stack logic to validate parentheses
-    pass
+    opening = ['(', '[', '{']
+    stack = []
+    for i in s:
+        if i in opening:
+            stack.append(i)
+        elif i == "}" and stack[len(stack)-1] == "{":
+            stack.pop()
+        elif i == ")" and stack[len(stack)-1] == "(":
+            stack.pop()
+        elif i == "]" and stack[len(stack)-1] == "[":
+            stack.pop()
+        else:
+            return False
+        
+    if stack == []:
+        return True
+    else:
+        return False
+
+tester = "({}[])"
+
+print(is_valid_parentheses(tester))
